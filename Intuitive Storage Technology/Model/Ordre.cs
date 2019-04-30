76bc977e45fa58e3dcc5075eacfa60b3d518b543
Emil Mosbæk Walsøe Pedersen <emil7213@edu.easj.dk>
2019-04-30 13:25:52 +0200

    Added props and ctor to Ordre and Organisation

--- Intuitive Storage Technology/Model/Ordre.cs
+++ Intuitive Storage Technology/Model/Ordre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,28 @@ namespace Intuitive_Storage_Technology.Model
 {
     class Ordre
     {
+        #region Properties
+
+        public int OrdreID { get; set; }
+        public int OrganisationID { get; set; }
+        public ObservableCollection<Vare> VareOC { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Note { get; set; }
+        public string Udleverer { get; set; }
+
+
+        #endregion
+
+        public Ordre(int ordreId, int organisationId, ObservableCollection<Vare> vareOc, DateTime dateTime, string note, string udleverer)
+        {
+            OrdreID = ordreId;
+            OrganisationID = organisationId;
+            VareOC = vareOc;
+            DateTime = dateTime;
+            Note = note;
+            Udleverer = udleverer;
+        }
+
+
     }
 }
